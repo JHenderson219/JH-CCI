0a96df2fa4317b603a2ef3479419fdc86de4569c
Jordan Henderson <hendersonj219@gmail.com>
2018-12-24 17:57:09 -0800

    In progress with 3.1 and 3.2

--- stacksAndQueues.js
+++ stacksAndQueues.js
@@ -65,31 +65,108 @@ Question 3.1
 Describe how you could use a single array to implement three stacks
 */
 
+// TODO: Fix this. It won't actually solve the problem as described.
+// class ArrStacks {
+//   constructor(arr) {
+//     this.arr = arr || [-2, -1, 0, 1, 2];
+//   }
+//   getStartIndex(stackNum) {
+//     switch (stackNum) {
+//       case 1:
+//         return 0;
+//       case 2:
+//         return this.arr.length - 1;
+//       case 3:
+//         return Math.floor(this.arr.length / 2);
+//       default:
+//         break;
+//     }
+//   }
+//   pop(stackNum) {
+//     const out = this.peek(stackNum);
+//     this.arr.splice(this.getStartIndex(stackNum, 1));
+//     return out;
+//   }
+//   push(value, stackNum) {
+//     this.arr.splice(this.getStartIndex(stackNum), 0, value);
+//   }
+//   peek(stackNum) {
+//     return this.arr[this.getStartIndex(stackNum)];
+//   }
+// }
+
 class ArrStacks {
   constructor(arr) {
-    this.arr = arr || [-2, -1, 0, 1, 2];
+    this.arr = arr || [-3, -2, -1, 0, 1, 2, 3];
+    this.bounds = this.calcBounds(arr) || [[0, 1], [2, 4], [5, 6]];
+  }
+  calcBounds(arr) {
+    if (!arr) {
+      return null;
+    }
+    const out = [];
+    const bound1 = Math.floor((arr.length - 1) / 3);
+    const bound2 = Math.floor(((arr.length - 1) / 3) * 2);
+    out.push([0, bound1]);
+    out.push([bound1 + 1, bound2]);
+    out.push([bound2 + 1, arr.length - 1]);
+    return out;
   }
   getStartIndex(stackNum) {
-    switch (stackNum) {
-      case 1:
-        return 0;
-      case 2:
-        return this.arr.length - 1;
-      case 3:
-        return Math.floor(this.arr.length / 2);
-      default:
-        break;
-    }
+    return this.bounds[stackNum][0];
+  }
+  getEndIndex(stackNum) {
+    return this.bounds[stackNum][1];
   }
   pop(stackNum) {
     const out = this.peek(stackNum);
-    this.arr.splice(this.getStartIndex(stackNum, 1));
+    if (!out) {
+      return null;
+    }
+    const startIndex = getStartIndex(stackNum);
+    const nextIndex = startIndex + 1;
+    if (nextIndex > this.bounds[stackNum][1]) {
+      // if we have an overflow
+    }
+    if (stackNum > 1) {
+      this.bounds[stackNum][0] = nextIndex;
+    }
+
+    this.arr.splice(startIndex, 1);
+    // update bounds
+    // delete element and resize array (splice)
     return out;
   }
   push(value, stackNum) {
-    this.arr.splice(this.getStartIndex(stackNum), 0, value);
+
   }
   peek(stackNum) {
     return this.arr[this.getStartIndex(stackNum)];
+  }
+}
+
+/*
+Question 3.2
+
+How would you design a stack which, in addition to push and pop, also has a function
+min which returns the minimum element? Push, pop and min should all operate in
+O(1) time.
+
+*/
+class StackWithMin extends Stack {
+  push(node) {
+    super.push(node);
+    if (!this.min) {
+      this.min = this.top;
+    }
+    if (this.min.value > this.top.value) {
+      this.min = this.top;
+    }
+  }
+  pop() {
+    const out = this.peek();
+  }
+  min() {
+    return this.min;
   }
 }
