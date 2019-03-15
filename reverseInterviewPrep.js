ecf34b3ceb9d31f427947888a88ef3f1642a8b63
Jordan Henderson <hendersonj219@gmail.com>
2019-03-14 23:01:10 -0700

    Implement swap and setup getMax

---
+++ reverseInterviewPrep.js
@@ -0,0 +1,26 @@
+
+/*
+Problem 19.1
+
+Write a function to swap a number in place without temporary variables
+*/
+
+function swap(firstInt, secondInt) {
+  firstInt = firstInt - secondInt;
+  secondInt = secondInt + firstInt;
+  firstInt = secondInt - firstInt;
+  return [firstInt, secondInt];
+}
+
+/*
+Problem 19.4
+
+Write a method which fnds the maximum of two numbers You should not use if-else or any other comparison operator
+*/
+
+
+function getMax(firstInt, secondInt) {
+  let max;
+  return max;
+}
+
